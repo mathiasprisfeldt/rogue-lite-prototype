c21a2d618445c4483c3c99ce3a805635ff1d20f6
Mathias Prisfeldt <contact@mathiasprisfeldt.me>
2017-08-07 16:21:06 +0200

    Started on fall dmg

---
+++ Assets/Scripts/FallDamage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Health
+{
+    enum FallDamageType
+    {
+        LastPosition,
+        Magnitude
+    }
+
+    /// <summary>
+    /// Purpose: Inflicts fall damage to a health system.
+    /// Creator: Mathias Prisfeldt
+    /// </summary>
+    [RequireComponent(typeof(Health))]
+    public class FallDamage : MonoBehaviour
+    {
+        private Health _health;
+
+        [SerializeField]
+        private FallDamageType _damageType;
+
+        [SerializeField]
+        private Rigidbody2D _rigidbody2D;
+
+        void Awake()
+        {
+            _health = GetComponent<Health>();
+        }
+    }
+}
