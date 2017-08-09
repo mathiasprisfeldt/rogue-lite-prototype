c9a866475ad5f92a5a05379f85c0135907f22b65
Peter Witt <peter.witt.joergensen@gmail.com>
2017-08-09 15:42:41 +0200

    Fixed some line endigns
    added my scene

--- Assets/Graphics/Tiled/Tileset/RogueLike.tsx
+++ Assets/Graphics/Tiled/Tileset/RogueLike.tsx
@@ -2,6 +2,6 @@
 <tileset name="ProjectRogueLike" tilewidth="100" tileheight="100" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="100" height="100" source="../../Sprites/Kaj.jpg"/>
+  <image width="100" height="100" source="../Sprites/Kaj.jpg"/>
  </tile>
 </tileset>
