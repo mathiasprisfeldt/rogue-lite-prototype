30ee6f67d1d8ea32a40ac8f54b418cf230e940e6
Peter Witt <peter.witt.joergensen@gmail.com>
2017-08-10 18:25:14 +0200

    Tiled files and sprites updated

--- Assets/Graphics/Tiled/Tileset/RogueLike.tsx
+++ Assets/Graphics/Tiled/Tileset/RogueLike.tsx
@@ -1,10 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="ProjectRogueLike" tilewidth="100" tileheight="100" tilecount="2" columns="0">
+<tileset name="ProjectRogueLike" tilewidth="280" tileheight="464" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="100" height="100" source="../Sprites/Kaj.jpg"/>
  </tile>
  <tile id="1">
-  <image width="70" height="70" source="castle.png"/>
+  <image width="256" height="256" source="../Sprites/Tiles/Tile (2).png"/>
+ </tile>
+ <tile id="2">
+  <image width="280" height="464" source="../Sprites/Tiles/DoorOpen.png"/>
  </tile>
 </tileset>
