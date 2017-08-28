2634a7c337ca20caab359c40c7b9e691624de92b
Mathias Prisfeldt <contact@mathiasprisfeldt.me>
2017-08-28 09:34:51 +0200

    Stopped Singleton from instantiating objects when the game is closing.
    This caused a error when closing down application.

--- Assets/AcrylecSkeleton/Utilities/Singleton.cs
+++ Assets/AcrylecSkeleton/Utilities/Singleton.cs
@@ -5,17 +5,22 @@ namespace AcrylecSkeleton.Utilities
 {
     public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
     {
+        private static bool _isQuitting;
+
         public bool DontDestroyOnLoadConfig;
 
         private static T instance;
 
         /**
        Returns the instance of this singleton.
-    */
+        */
         public static T Instance
         {
             get
             {
+                if (_isQuitting)
+                    return null;
+
                 if (instance == null)
                 {
                     instance = (T)FindObjectOfType(typeof(T));
@@ -54,6 +59,11 @@ namespace AcrylecSkeleton.Utilities
         {
             if (DontDestroyOnLoadConfig)
                 DontDestroyOnLoad(gameObject);
+        }
+
+        private void OnApplicationQuit()
+        {
+            _isQuitting = true;
         }
     }
 }
