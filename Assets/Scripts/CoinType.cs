3f0cb7196f015faafa02258d8c0fb9081eb2017d
Mikkel Bruun <mikkel.bn96@gmail.com>
2017-10-04 15:28:32 +0200

    Added coin spawner and coins. Added some simple money UI.

---
+++ Assets/Scripts/CoinType.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CoinTypes
+{
+    Gold, Silver, Bronze
+}
+
+public class CoinType : MonoBehaviour
+{
+    [SerializeField]
+    private CoinTypes _coinType;
+
+    [SerializeField]
+    private Animator _animator;
+
+    private CoinTypes _oldCoinType;
+	
+    private 
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (_coinType != _oldCoinType)
+        {
+            _oldCoinType = _coinType;
+            UpdateAnimator(_coinType);
+        }	
+	}
+
+    private void UpdateAnimator(CoinTypes coinType)
+    {
+        _animator.SetInteger("CoinType",(int)coinType);
+        _animator.SetTrigger("UpdateCoin");
+    }
+}
--- Assets/Scripts/PrefabDropper.cs
+++ Assets/Scripts/PrefabDropper.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Controllers;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace PrefabDropper
 {
@@ -7,13 +10,29 @@ namespace PrefabDropper
     /// Purpose:
     /// Creator:
     /// </summary>
-    public class PrefabDropper : MonoBehaviour 
+    public class PrefabDropper : MonoBehaviour
     {
         [SerializeField]
         private List<GameObject> _prefabs = new List<GameObject>();
 
-        [SerializeField,Range(0,360)]
+        [Tooltip("This value is used to determine how big " +
+                 "a chance the top object in the prefab list " +
+                 "is to drop. Every time the values isn't eached " +
+                 "randomly, then the object that will be spawned " +
+                 "will go down in the list until the last object")]
+        [SerializeField, Range(0, 1)]
+        private float _raityValue;
+
+        [SerializeField, Range(0, 360)]
         private float _dropAngle;
+
+        private Vector2 _angleDirection;
+
+        [SerializeField]
+        private int _minDropAmount;
+
+        [SerializeField]
+        private int _maxDropAmount;
 
         [SerializeField]
         private float _minForce;
@@ -21,34 +40,70 @@ namespace PrefabDropper
         [SerializeField]
         private float _maxForce;
 
+        [SerializeField, Range(0, 1)]
+        private float _changeToDrop;
+
+        [SerializeField]
+        private Character _character;
+
         [SerializeField]
         private bool _try;
 
-        [SerializeField,Range(0,1)]
-        private float _changeToDrop;
+        public void Awake()
+        {
+            if (_character && _character.HealthController)
+                _character.HealthController.OnDead.AddListener(OnDeath);
+        }
 
         public void Update()
         {
             if (_try)
             {
-                _try = false;
                 Drop();
+                _try = false;
             }
+        }
+
+        private void OnDeath()
+        {
+            Drop();
         }
 
         public void Drop()
         {
-            GameObject prefab = _prefabs[Random.Range(0,_prefabs.Count)];
-            GameObject instantiatedGameObject = Instantiate(prefab, transform.position, Quaternion.identity);
+            float randomValue = Random.Range(0f, 1f);
+            if (randomValue > _changeToDrop)
+                return;
 
-            Rigidbody2D rigigbody = instantiatedGameObject.GetComponent<Rigidbody2D>();
-            if (rigigbody)
+            int amountOfDrops = Random.Range(_minDropAmount, _maxDropAmount);
+
+            for (int i = 0; i < amountOfDrops; i++)
             {
-                var angle = Random.Range(0, _dropAngle) - _dropAngle/2;
-                Vector2 direction = new Vector2(Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle * Mathf.Deg2Rad));
-                var force = Random.Range(_minForce, _maxForce);
-                rigigbody.AddForce(direction * force,ForceMode2D.Impulse);
+                GameObject prefab = _prefabs[0];
+
+                for (var j = 0; j < _prefabs.Count; j++)
+                {
+                    randomValue = Random.Range(0f, 1f);
+                    if (j == _prefabs.Count - 1 || randomValue <= _raityValue)
+                    {
+                        prefab = _prefabs[j];
+                        break;
+                    }
+                }
+
+                GameObject instantiatedGameObject = Instantiate(prefab, transform.position, Quaternion.identity);
+
+                Rigidbody2D rigigbody = instantiatedGameObject.GetComponent<Rigidbody2D>();
+                if (rigigbody)
+                {
+                    var angle = Random.Range(0, _dropAngle) - _dropAngle / 2;
+                    Vector2 direction = new Vector2(Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle * Mathf.Deg2Rad));
+                    var force = Random.Range(_minForce, _maxForce);
+                    rigigbody.AddForce(direction * force, ForceMode2D.Impulse);
+                }
             }
+
+            
         }
     }
 }
