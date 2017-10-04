f12152bf62f4088b63953263a275968b06ea0c4f
Peter Witt <peter.witt.joergensen@gmail.com>
2017-10-04 16:33:23 +0200

    Worked on life steal

--- Assets/Objects/ItemSystem/LifestealItem/Lifesteal.cs
+++ Assets/Objects/ItemSystem/LifestealItem/Lifesteal.cs
@@ -3,17 +3,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Health;
+using Archon.SwissArmyLib.Utils;
+using CharacterController;
+using System.Linq;
 
 public class Lifesteal : Item
 {
     [SerializeField]
     private float _stealAmount;
 
+    [SerializeField]
+    private GameObject _gemPrefab;
+
+    [SerializeField]
+    private int _gemCount;
+
+    [SerializeField]
+    private float _gemRadius;
+
+    [SerializeField]
+    [Tooltip("Degrees per second")]
+    private float _gemSpeed = 10;
+
+    private Dictionary<int, LifestealGem> _gems = new Dictionary<int, LifestealGem>();
+
     public override void OnHit(HealthController victim)
     {
         base.OnHit(victim);
 
-        victim.Damage(_stealAmount, triggerItemhandler:false);
+        victim.Damage(_stealAmount, triggerItemhandler: false);
         ItemHandler.Owner.HealthController.Heal(_stealAmount);
+
+    }
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, _gemSpeed * BetterTime.DeltaTime));
+    }
+
+    public override void OnEquipped()
+    {
+        base.OnEquipped();
+
+        float angle = 360 / _gemCount;
+        for (int i = 0; i < _gemCount; i++)
+        {
+            _gems.Add(i, Instantiate(_gemPrefab,
+                PlaceOnCircle(transform.position, _gemRadius, angle * i),
+                Quaternion.identity, transform).GetComponent<LifestealGem>());
+        }
+
+        for (int i = 0; i < _gems.Count; i++)
+        {
+            _gems[i].Owner = ItemHandler.Owner;
+            _gems[i].Shoot();
+            _gems[i].LOwner = this;
+        }
+    }
+
+    private Vector3 PlaceOnCircle(Vector3 center, float radius, float ang)
+    {
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
     }
 }
---
+++ Assets/Objects/ItemSystem/LifestealItem/LifestealGem.cs
@@ -0,0 +1,65 @@
+﻿using AcrylecSkeleton.Utilities;
+using Archon.SwissArmyLib.Events;
+using Archon.SwissArmyLib.Utils;
+using Controllers;
+using Projectiles;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifestealGem : Projectile
+{
+    [Header("Lifesteal")]
+    [SerializeField]
+    private bool _spin;
+
+    [SerializeField]
+    private float _rotSpeed = 90;
+
+    [SerializeField]
+    private float _respawnTime = 5;
+
+    private SpriteRenderer _renderer;
+    private CircleCollider2D _collider;
+
+    public int index { get; set; }
+    public Lifesteal LOwner { get; set; }
+
+    private void Start()
+    {
+        _rotSpeed *= Random.value > .5f ? 1 : -1;
+        _collider = GetComponent<CircleCollider2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        transform.Rotate(new Vector3(0, 0, _rotSpeed * BetterTime.DeltaTime));
+    }
+
+    protected override void HandleDamage(CollisionCheck cc)
+    {
+        cc.Character.HealthController.Damage(_damage, pos: transform.position, from: Owner, triggerItemhandler: false);
+        Owner.HealthController.Heal(_damage);
+    }
+
+    protected override void Kill()
+    {
+        TellMeWhen.Seconds(_respawnTime, this);
+        _collider.enabled = false;
+        _renderer.enabled = false;
+    }
+
+    public override void OnTimesUp(int id, object args)
+    {
+        if (this)
+        {
+            _collider.enabled = true;
+            _renderer.enabled = true;
+        }
+    }
+
+
+}
