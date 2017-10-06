6700077e1cb7fbf69ceeeba563748c1025cad2d0
Peter Witt <peter.witt.joergensen@gmail.com>
2017-10-06 13:47:18 +0200

    Added random passives to bats

---
+++ Assets/Objects/Enemy/Enemies/Robobat/BatController.cs
@@ -0,0 +1,17 @@
+﻿using Controllers;
+using ItemSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatController : MonoBehaviour
+{
+    [SerializeField]
+    private List<Item> _randomItems = new List<Item>();
+
+    private void Awake()
+    {
+        GetComponent<Character>().ItemHandler.ItemsAtStart.Add(
+            _randomItems[Random.Range(0, _randomItems.Count)]);
+    }
+}
